83d95bf4f24d80395c4a9a2ffa0a7797ae089837
luyunyyyyy <516537801@qq.com>
2017-05-01 20:30:50 +0800

    增加了 教室实体类

---
+++ src/main/java/com/luyunyyyyy/service/ActicityRepository.java
@@ -0,0 +1,16 @@
+package com.luyunyyyyy.service;
+
+import com.luyunyyyyy.domain.Activity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.Collection;
+
+/**
+ * Created by LYY on 2017/5/1.
+ */
+public interface ActicityRepository extends JpaRepository<Activity, Long> {
+    @RestResource(path="activityClassroomId",rel="activityClassroomId")
+    Collection<Activity> findByActivityClassroomId(@Param("activityClassroomId") Long id);
+}
---
+++ src/main/java/com/luyunyyyyy/service/ClassroomRepository.java
@@ -0,0 +1,16 @@
+package com.luyunyyyyy.service;
+
+import com.luyunyyyyy.domain.Classroom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.Collection;
+
+/**
+ * Created by LYY on 2017/5/1.
+ */
+public interface ClassroomRepository extends JpaRepository<Classroom,Long>{
+    @RestResource(path="buildAndFloor",rel="buildAndFloor")
+    Collection<Classroom> findByBuildingNumberAndFloorNumber(@Param("buildingNumber") int buildingNumber,@Param("floorNumber") int floorNumber);
+}
