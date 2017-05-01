e043dc92dd3e5738d5337c5edede927d1e869465
luyunyyyyy <516537801@qq.com>
2017-05-01 11:38:42 +0800

    增加了 教室实体类

---
+++ src/main/java/com/luyunyyyyy/domain/Classroom.java
@@ -0,0 +1,60 @@
+package com.luyunyyyyy.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by LYY on 2017/5/1.
+ */
+@Entity
+public class Classroom {
+
+    @Id
+    @GeneratedValue
+    private long classroomId;
+    private String roomState;
+    private String roomSerialNumber;
+    private int buildingNumber;
+    private int floorNumber;
+
+    public long getClassroomId() {
+        return classroomId;
+    }
+
+    public void setClassroomId(long classroomId) {
+        this.classroomId = classroomId;
+    }
+
+    public String getRoomState() {
+        return roomState;
+    }
+
+    public void setRoomState(String roomState) {
+        this.roomState = roomState;
+    }
+
+    public String getRoomSerialNumber() {
+        return roomSerialNumber;
+    }
+
+    public void setRoomSerialNumber(String roomSerialNumber) {
+        this.roomSerialNumber = roomSerialNumber;
+    }
+
+    public int getBuildingNumber() {
+        return buildingNumber;
+    }
+
+    public void setBuildingNumber(int buildingNumber) {
+        this.buildingNumber = buildingNumber;
+    }
+
+    public int getFloorNumber() {
+        return floorNumber;
+    }
+
+    public void setFloorNumber(int floorNumber) {
+        this.floorNumber = floorNumber;
+    }
+}
