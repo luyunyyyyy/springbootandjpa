d56ec88bd03f798d1e01e632565147ebe114b719
luyunyyyyy <516537801@qq.com>
2017-05-18 16:24:56 +0800

    修改了main页面和对应jsp

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -53,8 +53,8 @@
 -- INSERT INTO `libdemo`.`classroom` (`building_number`, `floor_number`, `room_serial_number`) VALUES ('2', '2', 'A201');
 -- INSERT INTO `libdemo`.`classroom` (`building_number`, `floor_number`, `room_serial_number`) VALUES ('2', '3', 'A301');
 --
---
--- INSERT INTO `libdemo`.`activity` (`activity_classroom_id`, `activity_detail`) VALUES ('3', '教室id3 占用');
--- INSERT INTO `libdemo`.`activity` (`activity_classroom_id`, `activity_detail`) VALUES ('4', '教室id4 占用');
--- INSERT INTO `libdemo`.`activity` (`activity_classroom_id`, `activity_detail`) VALUES ('5', '教室id5 占用');
--- INSERT INTO `libdemo`.`activity` (`activity_classroom_id`, `activity_detail`) VALUES ('6', '教室id6 占用');
+
+INSERT INTO `libdemo`.`activity` (`activity_classroom_id`, `activity_detail`) VALUES ('3', '教室id3 占用');
+INSERT INTO `libdemo`.`activity` (`activity_classroom_id`, `activity_detail`) VALUES ('4', '教室id4 占用');
+INSERT INTO `libdemo`.`activity` (`activity_classroom_id`, `activity_detail`) VALUES ('5', '教室id5 占用');
+INSERT INTO `libdemo`.`activity` (`activity_classroom_id`, `activity_detail`) VALUES ('6', '教室id6 占用');
